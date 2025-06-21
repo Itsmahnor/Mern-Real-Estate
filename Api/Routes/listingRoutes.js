ed4d4f891ac3085b06a0692b8a820599b2bb0208
Itsmahnor <mahnoorsarfraz23@gmail.com>
2025-06-21 16:59:41 +0500

    search Api route

--- Api/Routes/listingRoutes.js
+++ Api/Routes/listingRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListing,deleteListing,getListing ,getSingleListing,updateListing} from "../Controller/listing.controller.js";
+import { createListing,getListings,deleteListing,getListing ,getSingleListing,updateListing} from "../Controller/listing.controller.js";
 import { VerifyToken } from "../utils/verifieduser.js";
 
 const router = express.Router();
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/create',VerifyToken,createListing)
 router.get('/getList/:id',VerifyToken,getListing)
 router.delete('/delete/:id',VerifyToken,deleteListing)
-
+router.get('/get',getListings)
 router.get('/single/:id', VerifyToken, getSingleListing);
 router.put('/update/:id', VerifyToken, updateListing);     
 
